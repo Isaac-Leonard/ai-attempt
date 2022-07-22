18d1f2e3c61ad5d272b6aa99f2acb935d236849f
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2022-07-23 00:38:54 +1000

    Add a kind of functional neural based AI that can find food to its left or right

--- src/brain.rs
+++ src/brain.rs
@@ -1,7 +1,9 @@
 use rand::Rng;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
-#[derive(Debug)]
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Cell {
     pub last_fired: u32,
     pub connections: Vec<usize>,
@@ -9,6 +11,7 @@ pub struct Cell {
     pub target: i32,
     pub adds: bool,
     pub reset_time: u32,
+    decay: u8,
 }
 impl Display for Cell {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
@@ -16,7 +19,7 @@ impl Display for Cell {
     }
 }
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Brain {
     pub cells: Vec<Cell>,
     pub connections: HashMap<(usize, usize), i16>,
@@ -29,85 +32,140 @@ impl Brain {
         };
         // Set up time processing
         for i in 0..128 {
-            let adds = i % 1000 > 50;
             brain.cells.push(Cell {
                 charge: 0,
-                adds,
-                connections: vec![
-                    prng.gen_range(128..1000000),
-                    prng.gen_range(128..1000000),
-                    prng.gen_range(128..1000000),
-                ],
+                adds: true,
+                connections: vec![],
                 target: 1,
                 last_fired: 0,
                 reset_time: 0,
+                decay: 2,
             })
         }
 
-        for i in 128..1000000 {
-            let adds = i % 1000 > 50;
-            brain.cells.push(Cell {
-                charge: 0,
-                adds,
-                connections: Vec::new(),
-                target: 1,
-                last_fired: 0,
-                reset_time: 2,
-            })
-        }
-        for i in 2..10000 {
-            for j in 0..100 {
-                brain.cells[i * 100 + j]
-                    .connections
-                    .push(prng.gen_range(0..100) + i * 100);
-                brain.cells[i * 100 + j]
-                    .connections
-                    .push(prng.gen_range(0..100) + i * 100);
-                brain.cells[i * 100 + j]
-                    .connections
-                    .push(prng.gen_range(0..100) + i * 100);
-            }
-        }
-        for i in 1..100 {
-            for j in 0..10000 {
-                brain.cells[i * 10000 + j].connections.append(&mut vec![
-                    prng.gen_range(0..10000) + i * 10000,
-                    prng.gen_range(0..10000) + i * 10000,
-                    prng.gen_range(0..10000) + i * 10000,
-                    prng.gen_range(0..10000) + i * 10000,
-                    prng.gen_range(0..10000) + i * 10000,
-                    prng.gen_range(0..10000) + i * 10000,
-                    prng.gen_range(0..10000) + i * 10000,
-                    prng.gen_range(0..10000) + i * 10000,
-                    prng.gen_range(128..1000000),
-                    prng.gen_range(128..1000000),
-                    (prng.gen_range(0..100000) + i * 10000) % 1000000,
-                ]);
-            }
-        }
+        brain.cells.push(Cell {
+            charge: 0,
+            adds: true,
+            connections: vec![],
+            target: 3,
+            last_fired: 0,
+            reset_time: 2,
+            decay: 6,
+        });
+
+        brain.cells.push(Cell {
+            charge: 0,
+            adds: true,
+            connections: vec![],
+            target: 3,
+            last_fired: 0,
+            reset_time: 2,
+            decay: 6,
+        });
+        brain.cells.push(Cell {
+            charge: 0,
+            adds: true,
+            connections: vec![],
+            target: 3,
+            last_fired: 0,
+            reset_time: 2,
+            decay: 6,
+        });
+        brain.cells.push(Cell {
+            charge: 0,
+            adds: true,
+            connections: vec![],
+            target: 3,
+            last_fired: 0,
+            reset_time: 2,
+            decay: 3,
+        });
+        brain.cells.push(Cell {
+            charge: 0,
+            adds: true,
+            connections: vec![],
+            target: 2,
+            last_fired: 0,
+            reset_time: 1,
+            decay: 3,
+        });
+        brain.cells.push(Cell {
+            charge: 0,
+            adds: true,
+            connections: vec![],
+            target: 1,
+            last_fired: 0,
+            reset_time: 3,
+            decay: 3,
+        });
+        brain.cells.push(Cell {
+            charge: 0,
+            adds: true,
+            connections: vec![],
+            target: 1,
+            last_fired: 0,
+            reset_time: 3,
+            decay: 6,
+        });
+        brain.cells.push(Cell {
+            charge: 0,
+            adds: true,
+            connections: vec![],
+            target: 1,
+            last_fired: 0,
+            reset_time: 3,
+            decay: 3,
+        });
+        brain.cells.push(Cell {
+            charge: 0,
+            adds: true,
+            connections: vec![],
+            target: 1,
+            last_fired: 0,
+            reset_time: 3,
+            decay: 6,
+        });
+        brain.cells[128].connections.push(131);
+        brain.cells[128].connections.push(132);
+        brain.cells[129].connections.push(131);
+        brain.cells[130].connections.push(132);
+        brain.cells[129].connections.push(133);
+        brain.cells[130].connections.push(134);
+        brain.cells[133].connections.push(131);
+        brain.cells[134].connections.push(132);
+        brain.cells[133].connections.push(135);
+        brain.cells[134].connections.push(136);
+        brain.cells[135].connections.push(131);
+        brain.cells[136].connections.push(132);
+
         brain
     }
 
     pub fn input(&mut self, inputs: Inputs) {
-        let time = std::time::SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .unwrap()
-            .as_millis()
-            .to_le_bytes();
-        for (i, byte) in time.iter().enumerate() {
-            let bits = u8_to_bools(*byte);
-            for (j, on) in bits.iter().enumerate() {
-                let loc = 100000 + i * 8 + j;
-                if *on {
-                    self.cells[loc].charge += 1
-                }
-            }
+        if inputs.hunger > 0 {
+            self.cells[128].charge += 1;
         }
-        for i in 0..1000 {}
+
+        if inputs.food < 0 {
+            self.cells[129].charge += 1;
+        }
+
+        if inputs.food > 0 {
+            self.cells[130].charge += 1;
+        }
+
         for i in 0..self.cells.len() {
             self.cells[i].last_fired += 1;
+            if self.cells[i].last_fired % self.cells[i].decay as u32 == 0 {
+                if self.cells[i].charge > 0 {
+                    self.cells[i].charge -= 1;
+                }
+                if self.cells[i].charge < 0 {
+                    self.cells[i].charge += 1
+                }
+            }
             if self.cells[i].charge >= self.cells[i].target
-                && self.cells[i].last_fired < self.cells[i].reset_time
+                && self.cells[i].last_fired > self.cells[i].reset_time
             {
                 for connection in self.cells[i].connections.clone() {
                     self.cells[connection].charge += 1;
@@ -117,17 +175,38 @@ impl Brain {
             }
         }
     }
+    fn fire_time(&mut self) {
+        let time = get_time();
+        for (i, byte) in time.iter().enumerate() {
+            let bits = u8_to_bools(*byte);
+            for (j, on) in bits.iter().enumerate() {
+                let loc = 100000 + i * 8 + j;
+                if *on {
+                    self.cells[loc].charge += 1
+                }
+            }
+        }
+    }
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Inputs {
-    pub current_line: Vec<u8>,
+    pub hunger: u8,
+    pub food: i8,
 }
 
 fn u8_to_bools(mut num: u8) -> [bool; 8] {
     let mut arr: [bool; 8] = [false, false, false, false, false, false, false, false];
     for i in 0..8 {
-        num = num >> i;
+        num >>= i;
         arr[i] = num & 1 > 0;
     }
     arr
+}
+fn get_time() -> [u8; 16] {
+    std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_millis()
+        .to_le_bytes()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,30 @@ mod brain;
 use brain::*;
 use rand::{prelude::*, rngs::*, Rng, SeedableRng};
 fn main() {
-    let rng = rand_chacha::ChaChaRng::seed_from_u64(0);
-    let mut brain = Brain::new(rng);
-    for _ in 0..1000 {
-        brain.input(Inputs {
-            current_line: Vec::new(),
-        });
-        println!("{}", brain.cells[104000]);
+    let mut rng = rand_chacha::ChaChaRng::seed_from_u64(0);
+    let mut brain = Brain::new(rng.clone());
+    let mut inputs = Inputs { hunger: 0, food: 0 };
+    let mut last_eat_time = 0;
+    let mut set = false;
+    for i in 0..1000 {
+        brain.input(inputs.clone());
+        if brain.cells[131].last_fired == 0 && set == true {
+            inputs.food += 1;
+        }
+        if brain.cells[132].last_fired == 0 && set == true {
+            inputs.food -= 1;
+        }
+        if inputs.food == 0 && set == true {
+            inputs.hunger = 0;
+            last_eat_time = i;
+            set = false;
+        }
+        if i - last_eat_time > 5 && set == false {
+            inputs.hunger = 1;
+            inputs.food = rng.gen_range(-25..25);
+            set = true;
+        }
+        println!("{}, {:?}", set, inputs);
     }
 }
 
